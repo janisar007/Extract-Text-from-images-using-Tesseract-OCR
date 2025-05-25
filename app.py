c186a2a9dc087beef39081d420c11f7dfbe0f532
janisar007 <134937141+janisar007@users.noreply.github.com>
2025-05-26 00:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def summarize():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=True)
